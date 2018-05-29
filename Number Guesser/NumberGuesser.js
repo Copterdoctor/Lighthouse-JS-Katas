83d89860e109cb496eed03face8447f82c25ed5e
Copterdoctor <jordananders@me.com>
2018-05-29 23:31:45 +0000

    finalize number guesser

--- Number Guesser/NumberGuesser.js
+++ Number Guesser/NumberGuesser.js
@@ -1,18 +1,22 @@
 var randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 var numberToGuess = randomNumber(0, 10);
+var attempts = 1;
 var prompt = require("prompt-sync")();
-console.log(numberToGuess);
 
 while (answer !== numberToGuess) {
-  var answer = prompt("Guess the number between 0 and 10 or enter x to exit\n>");
+  console.log("Guess the number between 0 and 10 or enter 66 to kill program.");
+  var answer = prompt("Attempt #" + attempts + ">");
   answer = parseInt(answer);
-  console.log(typeof(answer));
   if (answer === numberToGuess) {
     console.log ("Congratuations that is the correct answer");
+  } else if (answer === 66){
+    console.log("Goodbye");
+    break;
   } else if (answer < numberToGuess){
     console.log("Try guessing a higher number");
+    attempts++;
   } else if (answer > numberToGuess) {
     console.log("Try guessing a lower number");
-  } else
-    break;
+    attempts++;
+  }
 }
