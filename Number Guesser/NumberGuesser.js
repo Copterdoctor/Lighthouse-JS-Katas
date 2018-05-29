f828e56ed6787b9d4eecd7e133c45aa56d447503
Copterdoctor <jordananders@me.com>
2018-05-29 23:22:28 +0000

    Number Guesser add

--- Number Guesser/NumberGuesser.js
+++ Number Guesser/NumberGuesser.js
@@ -1,5 +1,18 @@
+var randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+var numberToGuess = randomNumber(0, 10);
 var prompt = require("prompt-sync")();
+console.log(numberToGuess);
 
-// code below (replace this example)
-var answer = prompt("What is your name? ");
-console.log("You answered: " + answer);
+while (answer !== numberToGuess) {
+  var answer = prompt("Guess the number between 0 and 10 or enter x to exit\n>");
+  answer = parseInt(answer);
+  console.log(typeof(answer));
+  if (answer === numberToGuess) {
+    console.log ("Congratuations that is the correct answer");
+  } else if (answer < numberToGuess){
+    console.log("Try guessing a higher number");
+  } else if (answer > numberToGuess) {
+    console.log("Try guessing a lower number");
+  } else
+    break;
+}
